f466615b1e2d21d4fea61ac9cc66db82e6676ea0
Sunil BT <sunil.rnsit@gmail.com>
2014-12-14 23:26:04 -0500

    Added OpenSSL
    I have added certificate and changed the URL calls from HTTP to HTTPS

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,2 +1,5 @@
 
+<h1>Welcome to the home/index view</h1>
+
+<p>Below is the result of the parameters passed into the URL.You can pass in the controller and model name too. Try it...<i>/home/index/[name]</i> or <i>/home/create/[name]/[email-id]</i></p>
 <h1>Welcome <b><?=$data['name']?></b></h1>
